adfa5a2b40095d1b18f0c2213e1c31d6d151b81d
Cordair <cordair@tuta.io>
2023-07-23 21:39:03 +0200

    Improved atbash_cipher to deal with alphanumerical characters and ignore the rest. Added comments and improved code. Updated code so it can work with command line arguments and deal with them in a basic way.

--- atbash_cipher.cpp
+++ atbash_cipher.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <cstring> // for argv[] in main()
 #include <iostream>
 
 // Note: there are 26 letters in the alphabet
@@ -44,15 +45,19 @@ std::string encode (std::string message){
     } 
 
     
- int main() {        
+ int main(int argc, char *argv[]) {
     
-     std::string encoded_message = encode("abcdefg");
-     
-     std::cout << encoded_message << std::endl;
-     
-     std::string decoded_message = decode("zyxwv ut");
-    
-     std::cout << decoded_message << std::endl;
-     
+    if (argc != 3) { // first value in argv is name of program, second is first argument and third is second argument
+        std::cout << argv[0] << " requires two arguments: first encode or decode and second the message to encode or decode." << std::endl;
+    } else if ( std::strcmp(argv[1], "encode") == 0 ) {
+        std::string encoded_message = encode(argv[2]); // if argv[2] is a C style string instead of a C++ string... should types match? encode is expecting a <string>
+        std::cout << encoded_message << std::endl;     
+    } else if ( std::strcmp(argv[1], "decode") == 0 ) {
+        std::string decoded_message = decode(argv[2]);
+        std::cout << decoded_message << std::endl;
+    } else {
+        std::cout << "Enter either 'encode' or 'decode' as first argument." << std::endl;
+    }
+
      return 0;
  }
